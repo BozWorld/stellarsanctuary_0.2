05d472632aaba360881d3b01659271d7f6bb0b0f
Boz <mondohamoeva@hotmail.fr>
2025-06-19 16:12:11 +0200

    [still cleaning]

--- scenes/snl_scene/snl_label.gd
+++
@@ -1,9 +0,0 @@
-extends RichTextLabel
-
-var full_text: String = ""
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-func _add_full_text(new_text: String) -> void:
-	full_text = new_text
